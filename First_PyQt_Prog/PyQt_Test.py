3ba01b0e86a9f2b81c5bc19abe7378baaa9d6b90
bsgmike <mike@mikebowley.plus.com>
2016-11-23 07:02:33 +0000

    learning python

---
+++ First_PyQt_Prog/PyQt_Test.py
@@ -0,0 +1,27 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import sys
+
+class HelloWorld(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        layout = QVBoxLayout()
+
+        label = QLabel("Hello World")
+        line_edit = QLineEdit()
+        button = QPushButton("Close")
+
+        layout.addWidget(label)
+        layout.addWidget(line_edit)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+        button.clicked.connect(self.close)
+        line_edit.textChanged.connect(label.setText)
+
+app = QApplication(sys.argv)
+dialog = HelloWorld()
+dialog.show()
+app.exec()
+
